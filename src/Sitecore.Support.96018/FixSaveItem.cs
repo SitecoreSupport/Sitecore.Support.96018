67c3eaf4ddc27aa7e4191287d6365b3d5d630065
AnnaAkimova <aak@sitecore.net>
2018-06-20 10:13:58 +0300

    Fix commit

---
+++ src/Sitecore.Support.96018/FixSaveItem.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.Support
+{
+  using Sitecore.Events.Hooks;
+  using Sitecore.SecurityModel;
+  using Sitecore.Data;
+  using Sitecore.Configuration;
+  using Sitecore.Data.Items;
+  using System;
+  using Sitecore.Diagnostics;
+
+  public class FixSaveItem : IHook
+  {
+    private string coreDbName;
+    public FixSaveItem(string coreDb)
+    {
+      Assert.ArgumentNotNullOrEmpty(coreDb, "coreDb");
+      this.coreDbName = coreDb;
+    }
+    public void Initialize()
+    {
+      Database coreDb = Factory.GetDatabase(coreDbName);
+      using (new SecurityDisabler())
+      {
+        try
+        {
+          Item saveButtonItem = coreDb.GetItem("{12FF26DE-2BBC-4E60-A43A-735DF841E3CA}");
+          using (new EditContext(saveButtonItem))
+          {
+            saveButtonItem.Fields[new ID("{CCD6BDE9-0A7A-4DA3-B0ED-D3C0F65379FE}")].Value = "";
+          }
+        }
+        catch (Exception e)
+        {
+          Log.Error("The error occurred while editing Save button item: ", e, this);
+        }
+      }
+    }
+  }
+}
